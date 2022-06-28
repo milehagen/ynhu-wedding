b1099d7b1351d68e3952f08e7c737ef9b4e7cca4
Mi <74912354+milehagen@users.noreply.github.com>
2022-06-28 20:27:04 +0200

    small changes

--- src/Components/Seating.js
+++ src/Components/Seating.js
@@ -1,79 +1,74 @@
 import React from "react";
 
-
 export function Seating() {
-	return (
-		<div class="container-xxl">
-			<br />
-			<div className="home-div">
-				<div class="container-lg">
-					<div class="row">
-						<div class="col-12">
-							<h2 className="find-your-seat-heading">Find Your Seat</h2>
-						</div>
-						<div class="col-4">
-							<h5 className="table-number-heading">Table 1</h5>
-							<ul className="guest-names">
-								<li>Alfa</li>
-								<li>beta</li>
-								<li>gamma</li>
-								<li>delta</li>
-								<li>epsilon</li>
-								<li>zeta</li>
-								<li>eta</li>
-								<li>theta</li>
-								<li>iota</li>
-								<li>kappa</li>
-								<li>lambda</li>
-								<li>mu</li>
-								<li>nu1</li>
-								<li>xi</li>
-								<li>omicron</li>
-							</ul>
-						</div>
-						<div class="col-4">
-							<h5 className="table-number-heading">Table 2</h5>
-							<ul>
-								<li>Alfa</li>
-								<li>beta</li>
-								<li>gamma</li>
-								<li>delta</li>
-								<li>epsilon</li>
-								<li>zeta</li>
-								<li>eta</li>
-								<li>theta</li>
-								<li>iota</li>
-								<li>kappa</li>
-								<li>lambda</li>
-								<li>mu</li>
-								<li>nu1</li>
-								<li>xi</li>
-								<li>omicron</li>
-							</ul>
-						</div>
-						<div class="col-4">
-							<h5 className="table-number-heading">Table 3</h5>
-							<ul>
-								<li>Alfa</li>
-								<li>beta</li>
-								<li>gamma</li>
-								<li>delta</li>
-								<li>epsilon</li>
-								<li>zeta</li>
-								<li>eta</li>
-								<li>theta</li>
-								<li>iota</li>
-								<li>kappa</li>
-								<li>lambda</li>
-								<li>mu</li>
-								<li>nu1</li>
-								<li>xi</li>
-								<li>omicron</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div class="container-xxl">
+      <br />
+      <div className="home-div">
+        <div class="container-lg">
+          <div class="row">
+            <div class="col-12">
+              <h2 className="find-seat">Find Your Seat</h2>
+            </div>
+            <div class="col-4">
+              <h5 className="table-number-heading">Table 1</h5>
+              <ul className="guest-names">
+                <li>AiVy</li>
+                <li>Chinh</li>
+                <li>Thy</li>
+                <li>Leah</li>
+                <li>Vy</li>
+                <li>Oddvar</li>
+                <li>Ronny</li>
+                <li>Caroline</li>
+                <li>Therese Nilsen</li>
+                <li>Bjørn</li>
+                <li>Amar</li>
+                <li>Sadia</li>
+                <li>Aryaan</li>
+              </ul>
+            </div>
+            <div class="col-4">
+              <h5 className="table-number-heading">Table 2</h5>
+              <ul>
+                <li>Lars </li>
+                <li>Geir</li>
+                <li>Nina</li>
+                <li>Nora Henriette</li>
+                <li>Ole Vegge</li>
+                <li>Eldar</li>
+                <li>Dì Năm</li>
+                <li>Cô Liên</li>
+                <li>Åge</li>
+                <li>Bill</li>
+                <li>Karen</li>
+                <li>Ingvild</li>
+                <li>Vibeke</li>
+              </ul>
+            </div>
+            <div class="col-4">
+              <h5 className="table-number-heading">Table 3</h5>
+              <ul>
+                <li>Alfa</li>
+                <li>beta</li>
+                <li>gamma</li>
+                <li>delta</li>
+                <li>epsilon</li>
+                <li>zeta</li>
+                <li>eta</li>
+                <li>theta</li>
+                <li>iota</li>
+                <li>kappa</li>
+                <li>lambda</li>
+                <li>mu</li>
+                <li>nu1</li>
+                <li>xi</li>
+                <li>omicron</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
