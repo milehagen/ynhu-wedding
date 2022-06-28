e056495b39b5c9fc0c9a23ac63ecdbe7f952e1c0
Mi <74912354+milehagen@users.noreply.github.com>
2022-06-28 22:46:24 +0200

    updated sizes for mobile

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,34 +4,26 @@ import "./Home.css";
 export function Home() {
 	return (
 		<div class="background-image">
-		<div class="container-xl">
-			<br />
-			<div className="home-div">
-				<div class="container">
-					<div class="row">
-						<div class="col-all-content">
-							<h2 className="cursive-text">Together with their families</h2>								
-							
-							<p className="nhu-magnus">
-								Y-NHU
-							</p>
-							<p className="cursive-text">
-								and
-							</p>
-							<p className="nhu-magnus">
-								MAGNUS
-							</p>
-							<p className="celebrate">
-								Look forward to celebrating their big day with you
-							</p>
-							<p className="date">
-								JUL | 9TH | 2022
-							</p>
+			<div class="container-xl">
+				<br />
+				<div className="home-div">
+					<div class="container">
+						<div class="row">
+							<div class="col-all-content">
+								<h2 className="cursive-text">Together with their families</h2>
+
+								<p className="nhu-magnus">Y-NHU</p>
+								<p className="cursive-text">and</p>
+								<p className="nhu-magnus">MAGNUS</p>
+								<p className="celebrate">
+									Look forward to celebrating their big day with you
+								</p>
+								<p className="date">JUL | 9TH | 2022</p>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
 		</div>
 	);
 }
