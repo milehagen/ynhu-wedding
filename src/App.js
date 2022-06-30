524c4548f7601a4cc1ce82745cb5d03e7507f63e
Mi <74912354+milehagen@users.noreply.github.com>
2022-06-30 14:16:02 +0200

    small changes

--- src/App.js
+++ src/App.js
@@ -6,26 +6,27 @@ import { Program } from "./Components/Program";
 import { Seating } from "./Components/Seating";
 
 export default function App() {
-	return (
-		<div class="container-fluid">
-			<Router>
-				<div class="container-lg">
-					<div className="App">
-						<Navbar />
-						<Switch>
-							<Route path="/home">
-								<Home />
-							</Route>
-							<Route path="/program">
-								<Program />
-							</Route>
-							<Route path="/seating">
-								<Seating />
-							</Route>
-						</Switch>
-					</div>
-				</div>
-			</Router>
-		</div>
-	);
+  return (
+    <div class="container-fluid">
+      <Router>
+        <Route path="/home"></Route>
+        <div class="container-lg">
+          <div className="App">
+            <Navbar />
+            <Switch>
+              <Route path="/home">
+                <Home />
+              </Route>
+              <Route path="/program">
+                <Program />
+              </Route>
+              <Route path="/seating">
+                <Seating />
+              </Route>
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    </div>
+  );
 }
