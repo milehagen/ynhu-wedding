c111a3d95ee61b6d2386ecf38dac627db2b19469
Mi <74912354+milehagen@users.noreply.github.com>
2022-06-30 13:13:56 +0200

    added background tone to seating

--- src/Components/Seating.js
+++ src/Components/Seating.js
@@ -2,311 +2,309 @@ import React from "react";
 import "./Seating.css";
 
 export function Seating() {
-	return (
-		<div className="background">
-			<div class="container-lg">
-				<div class="row">
-					<div class="col-12">
-						<h2 className="find-your-seat-heading">Find Your Seat</h2>
-					</div>
-					<div class="col-4">
-						<h5 className="table-number-heading">Table 1</h5>
-						<ul className="guest-names">
-							<li>AiVy </li>
-							<li>Chinh</li>
-							<li>Thy</li>
-							<li>Leah</li>
-							<li>Vy</li>
-							<li>Oddvar</li>
-							<li>Ronny</li>
-							<li>Caroline</li>
-							<li>Therese Nilsen</li>
-							<li>Bjørn</li>
-							<li>Amar</li>
-							<li>Sadia</li>
-							<li>Aryaan</li>
-						</ul>
-					</div>
-					<div class="col-4">
-						<h5 className="table-number-heading">Table 2</h5>
-						<ul>
-							<li>Lars </li>
-							<li>Geir</li>
-							<li>Nina</li>
-							<li>Nora Henriette</li>
-							<li>Ole Vegge</li>
-							<li>Eldar</li>
-							<li>Dì Năm</li>
-							<li>Cô Liên</li>
-							<li>Åge</li>
-							<li>Bill</li>
-							<li>Karen</li>
-							<li>Ingvild</li>
-							<li>Vibeke</li>
-						</ul>
-					</div>
-					<div class="col-4">
-						<h5 className="table-number-heading">Table 3</h5>
-						<ul>
-							<li>Mi </li>
-							<li>Chuma</li>
-							<li>Christian</li>
-							<li>Sarah</li>
-							<li>Chị Hoa (Asker)</li>
-							<li>Anh Hậu</li>
-							<li>Anh Phi Hùng</li>
-							<li>Chị Hảo</li>
-							<li>Hilde</li>
-							<li>Morten</li>
-							<li>Marius V. O. B</li>
-							<li>Mai Dinh</li>
-						</ul>
-					</div>
-					<div class="row">
-						<div class="col-4">
-							<h5 className="table-number-heading">Table 4</h5>
-							<ul>
-								<li>Bác Sự </li>
-								<li>Bác Cần</li>
-								<li>Cô Oanh</li>
-								<li>Chú Bằng</li>
-								<li>Chú Thảo </li>
-								<li>Cô Duyên </li>
-								<li>Cô Kim Anh</li>
-								<li>Chú Phước </li>
-								<li>Chú Khải </li>
-								<li>Cô Loan</li>
-								<li>Cô Hằng</li>
-								<li>Chú Phương </li>
-							</ul>
-						</div>
-						<div class="col-4">
-							<h5 className="table-number-heading">Table 5</h5>
-							<ul>
-								<li>Dante </li>
-								<li>Élodie</li>
-								<li>Amanda</li>
-								<li>Julie</li>
-								<li>Dustin </li>
-								<li>Bård Andreas</li>
-								<li>Ole Sparboe</li>
-								<li>Brudgom </li>
-								<li>Brud</li>
-								<li>Thanh</li>
-								<li>Loc</li>
-							</ul>
-						</div>
-						<div class="col-4">
-							<h5 className="table-number-heading">Table 6</h5>
-							<ul>
-								<li>Chị Nana </li>
-								<li>Phu Phu</li>
-								<li>Michael</li>
-								<li>Thieu-Nam</li>
-								<li>Blake </li>
-								<li>Yvonne</li>
-								<li>Therese Hansen</li>
-								<li>Linn-Elise </li>
-								<li>Magne</li>
-								<li> Anh Vũ</li>
-								<li>Anh Vĩnh </li>
-								<li>Louis</li>
-							</ul>
-						</div>
-						<div class="col-4">
-							<h5 className="table-number-heading">Table 7</h5>
-							<ul>
-								<li>Emma </li>
-								<li>Chị Hoa (Lørenskog) </li>
-								<li>Tido</li>
-								<li>Mai Dao</li>
-								<li>Therese Sagbakk </li>
-								<li> Chị Ngọc Cess</li>
-								<li>Chị Tina</li>
-								<li>Anh Tuấn (DK) </li>
-								<li>Anh Cường Chris</li>
-								<li>Như</li>
-							</ul>
-						</div>
-						<div class="col-4">
-							<h5 className="table-number-heading">Table 8</h5>
-							<ul>
-								<li>Chú Hóa </li>
-								<li>Cô Nhung</li>
-								<li>Chú Chung</li>
-								<li>Cô Trinh</li>
-								<li>Cô Đào</li>
-								<li> Dì Thời </li>
-								<li>Cô Thủy</li>
-								<li>Chú Hiệp </li>
-								<li>Dì Lan</li>
-								<li>Chú Dũng </li>
-							</ul>
-						</div>
-						<div class="col-4">
-							<h5 className="table-number-heading">Table 9</h5>
-							<ul>
-								<li>Gunn Kari </li>
-								<li>Gard</li>
-								<li>Bác Thành </li>
-								<li>Má Yến</li>
-								<li>Bà Tư </li>
-								<li>Bà Iá (iá)</li>
-								<li>Risalina</li>
-								<li>Werner </li>
-							</ul>
-						</div>
-						<div class="col-4">
-							<h5 className="table-number-heading">Table 10</h5>
-							<ul>
-								<li>Lia </li>
-								<li>Ivar</li>
-								<li>Eirik</li>
-								<li>Sigrun</li>
-								<li>Elisabeth </li>
-								<li>Marius C.</li>
-								<li>Martin</li>
-								<li>Runa </li>
-							</ul>
-						</div>
-						<div class="col-4">
-							<h5 className="table-number-heading">Table 11</h5>
-							<ul>
-								<li>Chú Hàn </li>
-								<li>Cô Thương</li>
-								<li>Cô Mai</li>
-								<li>Chú Phúc</li>
-								<li>Chú Khưởng </li>
-								<li>Cô Hồng</li>
-								<li> Cô Quyên</li>
-								<li>Bác Tài </li>
-							</ul>
-						</div>
-						<div class="col-4">
-							<h5 className="table-number-heading">Table 12</h5>
-							<ul>
-								<li>Thomas (FR) </li>
-								<li>Marie (FR) </li>
-								<li>Brandi</li>
-								<li>Øyvind</li>
-								<li>Henri </li>
-								<li>Anna</li>
-								<li>Unni</li>
-								<li>Tom Erik </li>
-								<li> Leah</li>
-								<li>Torbjørn</li>
-								<li>Chị Lệ</li>
-								<li>Lise</li>
-								<li>Jhon</li>
-								<li>Otto</li>
-								<li>Anu</li>
-								<li>Pierre</li>
-							</ul>
-						</div>
-						<div class="col-4">
-							<h5 className="table-number-heading">Table 13</h5>
-							<ul>
-								<li>Anh Quyền </li>
-								<li>Chị Đào</li>
-								<li>Duyên</li>
-								<li>Nam</li>
-								<li>Tâm </li>
-								<li>Hằng</li>
-								<li>Trâm</li>
-								<li>Lâm </li>
-								<li> Hoàng</li>
-								<li>Châu</li>
-								<li>Chị Phượng</li>
-								<li>Anh Nhu</li>
-							</ul>
-						</div>
-						<div class="col-4">
-							<h5 className="table-number-heading">Table 14</h5>
-							<ul>
-								<li>Magnus </li>
-								<li>Marianne</li>
-								<li>Inger-Lise </li>
-								<li>Ulrik</li>
-							</ul>
-						</div>
-						<div class="col-4">
-							<h5 className="table-number-heading">Table 15</h5>
-							<ul>
-								<li>Cô Thơ </li>
-								<li>Cô Bích</li>
-								<li>Cô Liễu </li>
-								<li>Bác Trang </li>
-							</ul>
-						</div>
-						<div class="col-4">
-							<h5 className="table-number-heading">Table 16</h5>
-							<ul>
-								<li>Chú Tường </li>
-								<li>Cô Thảo </li>
-								<li>Bác Thắng gái</li>
-								<li>Bác Thắng trai </li>
-							</ul>
-						</div>
-						<div class="col-4">
-							<h5 className="table-number-heading">Table 17</h5>
-							<ul>
-								<li>Bác Vĩnh </li>
-								<li>Cô Thoa </li>
-								<li> Cô Dung </li>
-								<li>Chú Nguyên </li>
-							</ul>
-						</div>
-						<div class="col-4">
-							<h5 className="table-number-heading">Table 18</h5>
-							<ul>
-								<li>Bjørn Kristian </li>
-								<li> Helle </li>
-								<li>Sander </li>
-								<li>Stian</li>
-							</ul>
-						</div>
-						<div class="col-4">
-							<h5 className="table-number-heading">Table 19</h5>
-							<ul>
-								<li>Signe </li>
-								<li> Elena </li>
-								<li>Laura </li>
-								<li>Aurora</li>
-								<li>Khai</li>
-								<li>Aaron</li>
-								<li>Emilyn</li>
-								<li>Alina May </li>
-							</ul>
-						</div>
-						<div class="col-4">
-							<h5 className="table-number-heading">Table 20</h5>
-							<ul>
-								<li>Tuva </li>
-								<li> Thomas </li>
-								<li>Lotta </li>
-								<li>Oliver</li>
-							</ul>
-						</div>
-						<div class="col-4">
-							<h5 className="table-number-heading">Table 21</h5>
-							<ul>
-								<li>Abel </li>
-								<li> Agustin </li>
-								<li>Tilde Ada-Marie </li>
-							</ul>
-						</div>
-						<div class="col-4">
-							<h5 className="table-number-heading">Table 22</h5>
-							<ul>
-								<li>Edith </li>
-								<li> Malene </li>
-								<li>Abel Johannes</li>
-								<li>Konrad</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="background">
+      <div class="container-lg">
+        <div class="row">
+          <div class="col-12">
+            <h2 className="find-your-seat-heading">Find Your Seat</h2>
+          </div>
+          <div class="col-4 beige">
+            <h5 className="table-number-heading">Table 1</h5>
+            <ul className="guest-names">
+              <li>AiVy </li>
+              <li>Chinh</li>
+              <li>Thy</li>
+              <li>Leah</li>
+              <li>Vy</li>
+              <li>Oddvar</li>
+              <li>Ronny</li>
+              <li>Caroline</li>
+              <li>Therese Nilsen</li>
+              <li>Bjørn</li>
+              <li>Amar</li>
+              <li>Sadia</li>
+              <li>Aryaan</li>
+            </ul>
+          </div>
+          <div class="col-4 light-beige">
+            <h5 className="table-number-heading">Table 2</h5>
+            <ul>
+              <li>Lars </li>
+              <li>Geir</li>
+              <li>Nina</li>
+              <li>Nora Henriette</li>
+              <li>Ole Vegge</li>
+              <li>Eldar</li>
+              <li>Dì Năm</li>
+              <li>Cô Liên</li>
+              <li>Åge</li>
+              <li>Bill</li>
+              <li>Karen</li>
+              <li>Ingvild</li>
+              <li>Vibeke</li>
+            </ul>
+          </div>
+          <div class="col-4 beige">
+            <h5 className="table-number-heading">Table 3</h5>
+            <ul>
+              <li>Mi </li>
+              <li>Chuma</li>
+              <li>Christian</li>
+              <li>Sarah</li>
+              <li>Chị Hoa (Asker)</li>
+              <li>Anh Hậu</li>
+              <li>Anh Phi Hùng</li>
+              <li>Chị Hảo</li>
+              <li>Hilde</li>
+              <li>Morten</li>
+              <li>Marius V. O. B</li>
+              <li>Mai Dinh</li>
+            </ul>
+          </div>
+          <div class="col-4 light-beige">
+            <h5 className="table-number-heading">Table 4</h5>
+            <ul>
+              <li>Bác Sự </li>
+              <li>Bác Cần</li>
+              <li>Cô Oanh</li>
+              <li>Chú Bằng</li>
+              <li>Chú Thảo </li>
+              <li>Cô Duyên </li>
+              <li>Cô Kim Anh</li>
+              <li>Chú Phước </li>
+              <li>Chú Khải </li>
+              <li>Cô Loan</li>
+              <li>Cô Hằng</li>
+              <li>Chú Phương </li>
+            </ul>
+          </div>
+          <div class="col-4 beige">
+            <h5 className="table-number-heading">Table 5</h5>
+            <ul>
+              <li>Dante </li>
+              <li>Élodie</li>
+              <li>Amanda</li>
+              <li>Julie</li>
+              <li>Dustin </li>
+              <li>Bård Andreas</li>
+              <li>Ole Sparboe</li>
+              <li>Brudgom </li>
+              <li>Brud</li>
+              <li>Thanh</li>
+              <li>Loc</li>
+            </ul>
+          </div>
+          <div class="col-4 light-beige">
+            <h5 className="table-number-heading">Table 6</h5>
+            <ul>
+              <li>Chị Nana </li>
+              <li>Phu Phu</li>
+              <li>Michael</li>
+              <li>Thieu-Nam</li>
+              <li>Blake </li>
+              <li>Yvonne</li>
+              <li>Therese Hansen</li>
+              <li>Linn-Elise </li>
+              <li>Magne</li>
+              <li> Anh Vũ</li>
+              <li>Anh Vĩnh </li>
+              <li>Louis</li>
+            </ul>
+          </div>
+          <div class="col-4 beige">
+            <h5 className="table-number-heading">Table 7</h5>
+            <ul>
+              <li>Emma </li>
+              <li>Chị Hoa (Lørenskog) </li>
+              <li>Tido</li>
+              <li>Mai Dao</li>
+              <li>Therese Sagbakk </li>
+              <li> Chị Ngọc Cess</li>
+              <li>Chị Tina</li>
+              <li>Anh Tuấn (DK) </li>
+              <li>Anh Cường Chris</li>
+              <li>Như</li>
+            </ul>
+          </div>
+          <div class="col-4 light-beige">
+            <h5 className="table-number-heading">Table 8</h5>
+            <ul>
+              <li>Chú Hóa </li>
+              <li>Cô Nhung</li>
+              <li>Chú Chung</li>
+              <li>Cô Trinh</li>
+              <li>Cô Đào</li>
+              <li> Dì Thời </li>
+              <li>Cô Thủy</li>
+              <li>Chú Hiệp </li>
+              <li>Dì Lan</li>
+              <li>Chú Dũng </li>
+            </ul>
+          </div>
+          <div class="col-4 beige">
+            <h5 className="table-number-heading">Table 9</h5>
+            <ul>
+              <li>Gunn Kari </li>
+              <li>Gard</li>
+              <li>Bác Thành </li>
+              <li>Má Yến</li>
+              <li>Bà Tư </li>
+              <li>Bà Iá (iá)</li>
+              <li>Risalina</li>
+              <li>Werner </li>
+            </ul>
+          </div>
+          <div class="col-4 light-beige">
+            <h5 className="table-number-heading">Table 10</h5>
+            <ul>
+              <li>Lia </li>
+              <li>Ivar</li>
+              <li>Eirik</li>
+              <li>Sigrun</li>
+              <li>Elisabeth </li>
+              <li>Marius C.</li>
+              <li>Martin</li>
+              <li>Runa </li>
+            </ul>
+          </div>
+          <div class="col-4 beige">
+            <h5 className="table-number-heading">Table 11</h5>
+            <ul>
+              <li>Chú Hàn </li>
+              <li>Cô Thương</li>
+              <li>Cô Mai</li>
+              <li>Chú Phúc</li>
+              <li>Chú Khưởng </li>
+              <li>Cô Hồng</li>
+              <li> Cô Quyên</li>
+              <li>Bác Tài </li>
+            </ul>
+          </div>
+          <div class="col-4 light-beige">
+            <h5 className="table-number-heading">Table 12</h5>
+            <ul>
+              <li>Thomas (FR) </li>
+              <li>Marie (FR) </li>
+              <li>Brandi</li>
+              <li>Øyvind</li>
+              <li>Henri </li>
+              <li>Anna</li>
+              <li>Unni</li>
+              <li>Tom Erik </li>
+              <li> Leah</li>
+              <li>Torbjørn</li>
+              <li>Chị Lệ</li>
+              <li>Lise</li>
+              <li>Jhon</li>
+              <li>Otto</li>
+              <li>Anu</li>
+              <li>Pierre</li>
+            </ul>
+          </div>
+          <div class="col-4 beige">
+            <h5 className="table-number-heading">Table 13</h5>
+            <ul>
+              <li>Anh Quyền </li>
+              <li>Chị Đào</li>
+              <li>Duyên</li>
+              <li>Nam</li>
+              <li>Tâm </li>
+              <li>Hằng</li>
+              <li>Trâm</li>
+              <li>Lâm </li>
+              <li> Hoàng</li>
+              <li>Châu</li>
+              <li>Chị Phượng</li>
+              <li>Anh Nhu</li>
+            </ul>
+          </div>
+          <div class="col-4 light-beige">
+            <h5 className="table-number-heading">Table 14</h5>
+            <ul>
+              <li>Magnus </li>
+              <li>Marianne</li>
+              <li>Inger-Lise </li>
+              <li>Ulrik</li>
+            </ul>
+          </div>
+          <div class="col-4 beige">
+            <h5 className="table-number-heading">Table 15</h5>
+            <ul>
+              <li>Cô Thơ </li>
+              <li>Cô Bích</li>
+              <li>Cô Liễu </li>
+              <li>Bác Trang </li>
+            </ul>
+          </div>
+          <div class="col-4 light-beige">
+            <h5 className="table-number-heading">Table 16</h5>
+            <ul>
+              <li>Chú Tường </li>
+              <li>Cô Thảo </li>
+              <li>Bác Thắng gái</li>
+              <li>Bác Thắng trai </li>
+            </ul>
+          </div>
+          <div class="col-4 beige">
+            <h5 className="table-number-heading">Table 17</h5>
+            <ul>
+              <li>Bác Vĩnh </li>
+              <li>Cô Thoa </li>
+              <li> Cô Dung </li>
+              <li>Chú Nguyên </li>
+            </ul>
+          </div>
+          <div class="col-4 light-beige">
+            <h5 className="table-number-heading">Table 18</h5>
+            <ul>
+              <li>Bjørn Kristian </li>
+              <li> Helle </li>
+              <li>Sander </li>
+              <li>Stian</li>
+            </ul>
+          </div>
+          <div class="col-4 beige">
+            <h5 className="table-number-heading">Table 19</h5>
+            <ul>
+              <li>Signe </li>
+              <li> Elena </li>
+              <li>Laura </li>
+              <li>Aurora</li>
+              <li>Khai</li>
+              <li>Aaron</li>
+              <li>Emilyn</li>
+              <li>Alina May </li>
+            </ul>
+          </div>
+          <div class="col-4 light-beige">
+            <h5 className="table-number-heading">Table 20</h5>
+            <ul>
+              <li>Tuva </li>
+              <li> Thomas </li>
+              <li>Lotta </li>
+              <li>Oliver</li>
+            </ul>
+          </div>
+          <div class="col-4 beige">
+            <h5 className="table-number-heading">Table 21</h5>
+            <ul>
+              <li>Abel </li>
+              <li> Agustin </li>
+              <li>Tilde Ada-Marie </li>
+            </ul>
+          </div>
+          <div class="col-4 light-beige">
+            <h5 className="table-number-heading">Table 22</h5>
+            <ul>
+              <li>Edith </li>
+              <li> Malene </li>
+              <li>Abel Johannes</li>
+              <li>Konrad</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
