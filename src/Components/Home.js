72eb1b5b2e91372251c1d5d9e957ab59b5f2ab02
Mi <74912354+milehagen@users.noreply.github.com>
2022-06-30 13:00:15 +0200

    added  card

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,29 +1,13 @@
 import React from "react";
 import "./Home.css";
+import img from "./../images/home-card.png";
 
 export function Home() {
-	return (
-		<div class="background-image">
-			<div class="container-xl">
-				<br />
-				<div className="home-div">
-					<div class="container">
-						<div class="row">
-							<div class="col-all-content">
-								<h2 className="cursive-text">Together with their families</h2>
-
-								<p className="nhu-magnus">Y-NHU</p>
-								<p className="cursive-text">and</p>
-								<p className="nhu-magnus">MAGNUS</p>
-								<p className="celebrate">
-									Look forward to celebrating their big day with you
-								</p>
-								<p className="date">JUL | 9TH | 2022</p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div class="container-fluid">
+      <div className="info-card-div">
+        <img src={img} alt="card" className="wedding-card" />
+      </div>
+    </div>
+  );
 }
