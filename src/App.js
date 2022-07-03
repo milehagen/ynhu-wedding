2fddb5808fbae962ad43c2f39ef6d7df12022e13
Mi <74912354+milehagen@users.noreply.github.com>
2022-07-03 15:51:03 +0200

    some changes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,41 @@
 import "./App.css";
 import { Navbar } from "./Components/Navbar";
 import { Home } from "./Components/Home";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+	BrowserRouter as Router,
+	Redirect,
+	Route,
+	Switch,
+} from "react-router-dom";
 import { Program } from "./Components/Program";
 import { Seating } from "./Components/Seating";
 import { Directions } from "./Components/Directions";
 
 export default function App() {
-  return (
-    <div class="container-fluid">
-      <Router>
-        <div class="container-xxl">
-          <div className="App">
-            <Navbar />
-            <Switch>
-              <Route path="/home">
-                <Home />
-              </Route>
-              <Route path="/program">
-                <Program />
-              </Route>
-              <Route path="/seating">
-                <Seating />
-              </Route>
-              <Route path="/directions">
-                <Directions />
-              </Route>
-            </Switch>
-          </div>
-        </div>
-      </Router>
-    </div>
-  );
+	return (
+		<div class="container-fluid">
+			<Router>
+				<div class="container-xxl">
+					<div className="App">
+						<Navbar />
+						<Switch>
+							<Route path="/home">
+								<Home />
+							</Route>
+							<Route path="/program">
+								<Program />
+							</Route>
+							<Route path="/seating">
+								<Seating />
+							</Route>
+							<Route path="/directions">
+								<Directions />
+							</Route>
+							<Redirect from="*" to="/home" />
+						</Switch>
+					</div>
+				</div>
+			</Router>
+		</div>
+	);
 }
